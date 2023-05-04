5bc3ed85586d3324912f03dceced8a7e80c5238f
Miguel Mejia <miguelmejiap05@hotmail.com>
2023-05-03 21:21:50 -0500

    Used the fixture file in the test and added assertions to it

--- cypress/e2e/latlong.cy.ts
+++ cypress/e2e/latlong.cy.ts
@@ -2,10 +2,15 @@ import { LandingPage } from "../pages/index";
 import places from '../fixtures/places.json';
 
 const landingPage = new LandingPage();
+//The coordinates for the cities are probably not right because i got the max daily quota from the page so i had to search them on google
+const cities = places.cities
 
 describe("Check coordinates", () => {
-    it("Should provide the right coordinates from a place with its given name", () => {
-        landingPage.visitLandingPage();
-        landingPage.getCoordinates(places.placeName1);
+    cities.forEach((city) => {
+        it("Should provide the right coordinates from a place with its given name", () => {
+            landingPage.visitLandingPage();
+            landingPage.getCoordinates(city.placeName);
+            landingPage.checkCoordinates(city.latitude, city.longitude);
+        });
     });
 });
