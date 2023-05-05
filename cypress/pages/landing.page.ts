e4b84bc5543349afea9459f945fb97116dd63ad7
Miguel Mejia <miguelmejiap05@hotmail.com>
2023-05-04 20:04:13 -0500

    Waits strategy implementation

--- cypress/pages/landing.page.ts
+++ cypress/pages/landing.page.ts
@@ -23,11 +23,17 @@ class LandingPage {
   }
 
   public getCoordinates(placeName: string): void {
+    //My apologies for implementing this, i wanted to wait for a specific request but it contained an unhandled exception, so i ignored just once for the purpose of this test
+    cy.once('uncaught:exception', () => false);
+    cy.get(this.placeNameTextBox).should('be.visible');
+    cy.get(this.placeNameTextBox).focus();
     cy.get(this.placeNameTextBox).type(placeName);
+    cy.intercept(/\/reload\?/).as('reload');
     cy.get(this.findButton).click();
   }
 
   public checkCoordinates(latitude: string, longitude : string): void {
+    cy.wait('@reload');
     cy.get(this.latitudeTextBox).should('have.value', latitude);
     cy.get(this.longitudeTextBox).should('have.value', longitude);
   }
